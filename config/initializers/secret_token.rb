a741df47c26e2e513068ce371a7fd79a7974bde1
jeffrossif <tuxpqp@gmail.com>
2013-02-12 15:27:50 -0200

    first att all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WellTalk::Application.config.secret_token = '1171f163a008e79243014090ae2db8352bf4d50d3aca96e82e53b84ea50d6ab6e17d338cf82d371049d4843b80102ebadabadc6e573108ea1b8e66d16dbe1736'
